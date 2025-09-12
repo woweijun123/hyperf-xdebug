5c44fa82b70c3f4e37bd8d1a987fa96046bc38ca
weichengjun <1054487195@qq.com>
2025-09-12 13:48:04 +0800

    edit

--- publish/xdebug.php
+++ publish/xdebug.php
@@ -1,19 +1,17 @@
 <?php
 declare(strict_types=1);
-/**
- * @author   riven <https://github.com/riven>
- * @contact  riven@qq.com
- */
+
+use function Hyperf\Support\env;
 
 return [
-    // the php path.
-    // e.g. /usr/bin/php
-    'bin_path' => 'php',
-    // -d xdebug.client_host="127.0.0.1" -d xdebug.client_port=9000 -d xdebug.mode=debug
+    'bin_path' => 'php', // PHP 可执行文件的路径，通常就是 'php'
     'xdebug' => [
-        'client_host' => \Hyperf\Support\env('XDEBUG_CLIENT_HOST','127.0.0.1'),
-        'client_port' => (int)\Hyperf\Support\env('XDEBUG_CLIENT_POST',9000),
+        'client_host' => env('XDEBUG_CLIENT_HOST', '127.0.0.1'), // IDE 所在机器的 IP
+        // 如果 Hyperf 运行在 Docker 或虚拟机内，需设置为宿主机的 IP
+        // 例如 Docker 内可设置为 'host.docker.internal'
+        // 例如 WSL 内可设置为宿主机对应 vEthernet (WSL) 的 IPv4 地址
+        'client_port' => (int)env('XDEBUG_CLIENT_PORT', 9000), // PhpStorm 监听的调试端口
         'mode' => 'debug',
     ],
-    'server_name' => \Hyperf\Support\env('XDEBUG_SERVER_NAME','Unnamed'),
+    'server_name' => env('XDEBUG_SERVER_NAME', 'Unnamed'), // 在 PhpStorm 中配置的 Server Name
 ];
