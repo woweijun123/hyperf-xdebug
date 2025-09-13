8c219a84fbe24e743294d87fb754b915599f5650
weichengjun <1054487195@qq.com>
2025-09-13 11:38:02 +0800

    docs(xdebug): 更新 xdebug配置文档
    - 调整配置项的顺序，使 client_host 在 client_port 之前
    - 在 client_host 配置项的注释中添加 Docker 和 WSL 的示例
    - 优化注释内容，使其更加清晰易懂

--- publish/xdebug.php
+++ publish/xdebug.php
@@ -6,11 +6,12 @@ use function Hyperf\Support\env;
 return [
     'bin_path' => 'php', // PHP 可执行文件的路径，通常就是 'php'
     'xdebug' => [
-        'client_host' => env('XDEBUG_CLIENT_HOST', '127.0.0.1'), // IDE 所在机器的 IP
-        // 如果 Hyperf 运行在 Docker 或虚拟机内，需设置为宿主机的 IP
+        // IDE 所在机器的 IP 「如果 Hyperf 运行在 Docker 或虚拟机内，需设置为宿主机的 IP」
         // 例如 Docker 内可设置为 'host.docker.internal'
         // 例如 WSL 内可设置为宿主机对应 vEthernet (WSL) 的 IPv4 地址
-        'client_port' => (int)env('XDEBUG_CLIENT_PORT', 9000), // PhpStorm 监听的调试端口
+        'client_host' => env('XDEBUG_CLIENT_HOST', '127.0.0.1'),
+        // PhpStorm 监听的调试端口
+        'client_port' => (int)env('XDEBUG_CLIENT_PORT', 9000),
         'mode' => 'debug',
     ],
     'server_name' => env('XDEBUG_SERVER_NAME', 'Unnamed'), // 在 PhpStorm 中配置的 Server Name
